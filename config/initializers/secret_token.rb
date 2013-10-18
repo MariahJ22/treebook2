b40e19ec041784e0ade74aa12fc306fedb551512
Mariah Jacobs <mariah.jacobs22@gmail.com>
2013-10-17 21:09:14 -0700

    Added interactivity to treebook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'f00cab1fdc1cd38c19379323da5bcc7625c5e8cb00a3de8658cb23efb7bdb637824fe3d78341a460d2dc6da9627b0ba774020104750190400108130d8cf0bed8'
---
+++ treebook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '27bd8f7413c0e39fd170267c72887d5ab27b8174bb63289330e100b580882d9343ea171270cfe75f6a82f00c0488168e4a38dba189789db40ae7e74c8b974ddc'
